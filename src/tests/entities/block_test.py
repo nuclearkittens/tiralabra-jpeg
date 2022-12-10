fix calculation error in block module; test pad removal

src/tests/entities/block_test.py
@@ -6,7 +6,7 @@ from entities.block import Block
 class TestBlock(unittest.TestCase):
     def setUp(self):
         self.test_mat1 = np.random.randint(256, size=(8, 8))
-        self.test_mat2 = np.random.randint(256, size=(16, 8))
+        self.test_mat2 = np.random.randint(256, size=(15, 12))
         self.test_im1 = np.dstack((self.test_mat1, self.test_mat1, self.test_mat1))
         self.test_im2 = np.dstack((self.test_mat2, self.test_mat2, self.test_mat2))
         self.test_block = Block()
@@ -18,21 +18,19 @@ class TestBlock(unittest.TestCase):
         self.assertEqual(self.test_block._hpad, 0)
 
     def test_vertical_pad(self):
-        pad = 1
-        padded = self.test_block._vertical_pad(self.test_im1)
-        h = self.test_im1.shape[0]
+        padded = self.test_block._vertical_pad(self.test_im2)
+        h = self.test_im2.shape[0]
         padded_h = padded.shape[0]
         self.assertTrue(padded_h-h == self.test_block._vpad)
-        self.assertEqual(self.test_im1[:,-1].all(), padded[:,-1].all())
+        self.assertEqual(self.test_im2[:,-1].all(), padded[:,-1].all())
         self.assertEqual(padded[:,-1].all(), padded[:,-2].all())
 
     def test_horizontal_pad(self):
-        pad = 1
-        padded = self.test_block._horizontal_pad(self.test_im1)
-        w = self.test_im1.shape[1]
+        padded = self.test_block._horizontal_pad(self.test_im2)
+        w = self.test_im2.shape[1]
         padded_w = padded.shape[1]
         self.assertTrue(padded_w-w == self.test_block._hpad)
-        self.assertEqual(self.test_im1[-1].all(), padded[-1].all())
+        self.assertEqual(self.test_im2[-1].all(), padded[-1].all())
         self.assertEqual(padded[-1].all(), padded[-2].all())
 
     def test_pad_no_pad(self):
@@ -43,3 +41,7 @@ class TestBlock(unittest.TestCase):
         new_w = new_im.shape[1]
         self.assertEqual((orig_h, orig_w, self.test_im1), (new_h, new_w, new_im))
 
+    def test_remove_pad(self):
+        padded = self.test_block.pad(self.test_im2)
+        unpadded = self.test_block.remove_pad(padded)
+        self.assertEqual(self.test_im2.all(), padded.all())